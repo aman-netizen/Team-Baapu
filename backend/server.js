9e7ecf81521c5420c7636e7b68bd5d277c42d068
Aman Yadav <amanyadav04st@gmail.com>
2023-01-16 08:09:14 +0530

    getCommit

--- backend/server.js
+++ backend/server.js
@@ -5,16 +5,21 @@ const db = require("./config/db");
 app.use(express.json());
 const userRoute = require('./routes/userRoutes');
 const companyRoute = require("./routes/companyRoute");
-const uploadCSVRoute = require('./routes/uploadCSVRoute')
+// const uploadCSVRoute = require('./routes/uploadCSVRoute')
 const cors = require("cors");
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
 app.use(cors());
 
+
 app.use("/api/users", userRoute);
 app.use("/api/companyData", companyRoute);
 // app.use("/api/csv", uploadCSVRoute);
+
+api.get('/', (req, res) => {
+  res.send("welcome to this universe");
+})
 
 const port = process.env.PORT || 8080;
 
